576d006d82ba2b69f05afb83707272e6c13597c2
ReaSlyn <reaslynn@gmail.com>
2021-11-17 14:25:06 +0100

    V1 Safari status bar bug fix

--- pages/_app.js
+++ pages/_app.js
@@ -42,6 +42,8 @@ function MyApp({Component, pageProps}) {
       <Head>
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="mobile-web-app-capable" content="yes" />
         <meta name="viewport" content="width=device-width,initial-scale=1.0" />
         <meta
           property="og:title"
