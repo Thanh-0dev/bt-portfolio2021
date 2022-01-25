108c4a80e2047c3d2c5b01cbe594ea5b951e2b38
ReaSlyn <reaslynn@gmail.com>
2022-01-25 23:12:40 +0100

    Fixed loading on mobile

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ import Layout from "/components/layout/Layout";
 import Loader from "/components/Loader";
 import Transition from "/components/Transition";
 import "/styles/globals.css";
+import {useRouter} from "next/router";
 
 function MyApp({Component, pageProps}) {
   /* Cursor */
@@ -80,6 +81,7 @@ function MyApp({Component, pageProps}) {
   });
 
   /* Loading screen */
+  const router = useRouter();
   const [firstTime, setFirstTime] = useState(true);
   const [leave, setLeave] = useState(false);
   useEffect(() => {
@@ -91,7 +93,7 @@ function MyApp({Component, pageProps}) {
         }, 400);
       }, 2100);
     });
-  }, []);
+  }, [router]);
 
   /* Page transition effect */
   const [loading, setLoading] = useState(false);
