629f87321489a8a9116ff5cff43b5146d57d1b88
Thanh-Long Pham <thanh@theoffline.fr>
2022-09-11 19:31:33 +0200

    changed link

--- components/home/Footer.jsx
+++ components/home/Footer.jsx
@@ -1,62 +1,62 @@
-import styles from "./Footer.module.css";
+import styles from './Footer.module.css';
 
 /* Footer section of the homepage */
 function Footer(props) {
-  return (
-    <section className={styles.footer}>
-      {/* Background decoration */}
-      <img
-        className={styles.footerLine}
-        src="/Icons habillage/Footer-line.svg"
-        alt="Footer Line"
-      />
-      <div className={styles.info}>
-        <img
-          className={styles.footerGlass1}
-          src="/Icons habillage/Glass/glass-footer-1.svg"
-          alt="Glass footer 1"
-        />
-        <div className={styles.footerGlass2}>
-          <img
-            src="/Icons habillage/Glass/glass-footer-2.svg"
-            alt="Glass footer 2"
-          />
-        </div>
-        <div className={styles.blur}>
-          <img src="/Icons habillage/Blur.png" alt="Blur" />
-        </div>
-        {/* Modulable content within Prisma Studio */}
-        <p>{props.footer[0].firstP}</p>
-        <p>{props.footer[0].secondP}</p>
-        <div className={styles.contact}>
-          {/* Links / Mails */}
-          <a
-            className="noTransition"
-            href="mailto:bichtrampham.design@gmail.com"
-            rel="noreferrer noopener nofollow"
-          >
-            /email
-          </a>
-          <a
-            className="noTransition"
-            href="https://www.linkedin.com/in/bichtrampham/"
-            target="_blank"
-            rel="noreferrer noopener nofollow"
-          >
-            /linkedIn
-          </a>
-          <a
-            className="noTransition"
-            href="https://www.instagram.com/tram.inprogress/"
-            target="_blank"
-            rel="noreferrer noopener nofollow"
-          >
-            /instagram
-          </a>
-        </div>
-      </div>
-    </section>
-  );
+	return (
+		<section className={styles.footer}>
+			{/* Background decoration */}
+			<img
+				className={styles.footerLine}
+				src="/Icons habillage/Footer-line.svg"
+				alt="Footer Line"
+			/>
+			<div className={styles.info}>
+				<img
+					className={styles.footerGlass1}
+					src="/Icons habillage/Glass/glass-footer-1.svg"
+					alt="Glass footer 1"
+				/>
+				<div className={styles.footerGlass2}>
+					<img
+						src="/Icons habillage/Glass/glass-footer-2.svg"
+						alt="Glass footer 2"
+					/>
+				</div>
+				<div className={styles.blur}>
+					<img src="/Icons habillage/Blur.png" alt="Blur" />
+				</div>
+				{/* Modulable content within Prisma Studio */}
+				<p>{props.footer[0].firstP}</p>
+				<p>{props.footer[0].secondP}</p>
+				<div className={styles.contact}>
+					{/* Links / Mails */}
+					<a
+						className="noTransition"
+						href="mailto:bichtrampham.design@gmail.com"
+						rel="noreferrer noopener nofollow"
+					>
+						/email
+					</a>
+					<a
+						className="noTransition"
+						href="https://www.linkedin.com/in/bichtrampham/"
+						target="_blank"
+						rel="noreferrer noopener nofollow"
+					>
+						/linkedIn
+					</a>
+					<a
+						className="noTransition"
+						href="https://www.instagram.com/b.aroundesign/"
+						target="_blank"
+						rel="noreferrer noopener nofollow"
+					>
+						/instagram
+					</a>
+				</div>
+			</div>
+		</section>
+	);
 }
 
 export default Footer;
--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -1,134 +1,136 @@
-import ActiveLink from "/components/ActiveLink";
-import {useRouter} from "next/router";
-import {useEffect} from "react";
-import styles from "./Header.module.css";
+import ActiveLink from '/components/ActiveLink';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import styles from './Header.module.css';
 
 /* Header of the site */
 function Header() {
-  /* Burger menu */
-  const router = useRouter();
-  useEffect(() => {
-    /* Open mobile menu on burger menu click and disable scrolling */
-    const burgerMenu = document.getElementById("burger");
-    const menu = document.getElementById("menu");
-    const html = document.documentElement;
-    let menuIsOpen = false;
-    burgerMenu.addEventListener("click", () => {
-      setTimeout(() => {
-        if (!menuIsOpen) {
-          menu.classList.add(styles.active);
-          html.classList.add("noScroll");
-        } else {
-          menu.classList.remove(styles.active);
-          html.classList.remove("noScroll");
-        }
-        menuIsOpen = !menuIsOpen;
-      }, 550);
-    });
-    const menuLinks = document.querySelectorAll("#menu a");
-    for (let link = 0; link < menuLinks.length; link++) {
-      /* Close mobile menu on page change */
-      if (menuLinks[link].href !== router.pathname) {
-        menuLinks[link].addEventListener("click", () => {
-          menuIsOpen = false;
-          menu.classList.remove(styles.active);
-          html.classList.remove("noScroll");
-        });
-      } else {
-        menuIsOpen = true;
-      }
-    }
-  }, [router.pathname]);
+	/* Burger menu */
+	const router = useRouter();
+	useEffect(() => {
+		/* Open mobile menu on burger menu click and disable scrolling */
+		const burgerMenu = document.getElementById('burger');
+		const menu = document.getElementById('menu');
+		const html = document.documentElement;
+		let menuIsOpen = false;
+		burgerMenu.addEventListener('click', () => {
+			setTimeout(() => {
+				if (!menuIsOpen) {
+					menu.classList.add(styles.active);
+					html.classList.add('noScroll');
+				} else {
+					menu.classList.remove(styles.active);
+					html.classList.remove('noScroll');
+				}
+				menuIsOpen = !menuIsOpen;
+			}, 550);
+		});
+		const menuLinks = document.querySelectorAll('#menu a');
+		for (let link = 0; link < menuLinks.length; link++) {
+			/* Close mobile menu on page change */
+			if (menuLinks[link].href !== router.pathname) {
+				menuLinks[link].addEventListener('click', () => {
+					menuIsOpen = false;
+					menu.classList.remove(styles.active);
+					html.classList.remove('noScroll');
+				});
+			} else {
+				menuIsOpen = true;
+			}
+		}
+	}, [router.pathname]);
 
-  useEffect(() => {
-    /* Get all links and show an underline under it if we're already on the page */
-    const menuLinks = document.querySelectorAll("#menu a");
-    for (let link = 0; link < menuLinks.length; link++) {
-      if (menuLinks[link].href === window.location.href) {
-        menuLinks[link].classList.add(styles.underline);
-      } else {
-        menuLinks[link].classList.remove(styles.underline);
-      }
-    }
-  });
+	useEffect(() => {
+		/* Get all links and show an underline under it if we're already on the page */
+		const menuLinks = document.querySelectorAll('#menu a');
+		for (let link = 0; link < menuLinks.length; link++) {
+			if (menuLinks[link].href === window.location.href) {
+				menuLinks[link].classList.add(styles.underline);
+			} else {
+				menuLinks[link].classList.remove(styles.underline);
+			}
+		}
+	});
 
-  /* Show time */
-  useEffect(() => {
-    setInterval(() => {
-      let date = new Date();
-      let time =
-        date.toLocaleTimeString("fr-FR", {hour: "2-digit", minute: "2-digit"}) +
-        " UTC+2";
-      document.getElementById("time").innerHTML = time;
-    }, 1000);
-  });
-  return (
-    <header>
-      <nav className={styles.nav}>
-        <div className={styles.burgerMenu} id="menu">
-          <ul>
-            {/* Mobile links */}
-            <ActiveLink href="/">
-              <li>/workshop</li>
-            </ActiveLink>
-            <ActiveLink href="/about">
-              <li>/statistics</li>
-            </ActiveLink>
-          </ul>
-          <ul>
-            {/* Social media links and mail */}
-            <a
-              className="noTransition"
-              href="mailto:bichtrampham.design@gmail.com"
-              rel="noreferrer noopener nofollow"
-            >
-              <li>/email</li>
-            </a>
-            <a
-              className="noTransition"
-              href="https://www.linkedin.com/in/bichtrampham/"
-              target="_blank"
-              rel="noreferrer noopener nofollow"
-            >
-              <li>/linkedIn</li>
-            </a>
-            <a
-              className="noTransition"
-              href="https://www.instagram.com/tram.inprogress/"
-              target="_blank"
-              rel="noreferrer noopener nofollow"
-            >
-              <li>/instagram</li>
-            </a>
-          </ul>
-        </div>
-        <ul>
-          {/* Links */}
-          <li>
-            <ActiveLink href="/">.tram/workshop</ActiveLink>
-          </li>
-          <li>
-            <ActiveLink href="/about">/statistics</ActiveLink>
-          </li>
-          <li>
-            <a>
-              <img
-                id="burger"
-                src="/Mobile/burger-menu.svg"
-                alt="burger menu"
-              />
-            </a>
-          </li>
-        </ul>
-        <div className={styles.info}>
-          {/* Information */}
-          <span>.actually in France</span>
-          <span>.open to opportunities</span>
-          <span id="time"></span>
-        </div>
-      </nav>
-    </header>
-  );
+	/* Show time */
+	useEffect(() => {
+		setInterval(() => {
+			let date = new Date();
+			let time =
+				date.toLocaleTimeString('fr-FR', {
+					hour: '2-digit',
+					minute: '2-digit',
+				}) + ' UTC+2';
+			document.getElementById('time').innerHTML = time;
+		}, 1000);
+	});
+	return (
+		<header>
+			<nav className={styles.nav}>
+				<div className={styles.burgerMenu} id="menu">
+					<ul>
+						{/* Mobile links */}
+						<ActiveLink href="/">
+							<li>/workshop</li>
+						</ActiveLink>
+						<ActiveLink href="/about">
+							<li>/statistics</li>
+						</ActiveLink>
+					</ul>
+					<ul>
+						{/* Social media links and mail */}
+						<a
+							className="noTransition"
+							href="mailto:bichtrampham.design@gmail.com"
+							rel="noreferrer noopener nofollow"
+						>
+							<li>/email</li>
+						</a>
+						<a
+							className="noTransition"
+							href="https://www.linkedin.com/in/bichtrampham/"
+							target="_blank"
+							rel="noreferrer noopener nofollow"
+						>
+							<li>/linkedIn</li>
+						</a>
+						<a
+							className="noTransition"
+							href="https://www.instagram.com/b.aroundesign/"
+							target="_blank"
+							rel="noreferrer noopener nofollow"
+						>
+							<li>/instagram</li>
+						</a>
+					</ul>
+				</div>
+				<ul>
+					{/* Links */}
+					<li>
+						<ActiveLink href="/">.tram/workshop</ActiveLink>
+					</li>
+					<li>
+						<ActiveLink href="/about">/statistics</ActiveLink>
+					</li>
+					<li>
+						<a>
+							<img
+								id="burger"
+								src="/Mobile/burger-menu.svg"
+								alt="burger menu"
+							/>
+						</a>
+					</li>
+				</ul>
+				<div className={styles.info}>
+					{/* Information */}
+					<span>.actually in France</span>
+					<span>.open to opportunities</span>
+					<span id="time"></span>
+				</div>
+			</nav>
+		</header>
+	);
 }
 
 export default Header;
